5e6464a9aa2c8579fdf32b43df6c212911f242a9
Mai Nguyen <mainv@vng.com.vn>
2024-07-04 23:59:19 +0700

    update code

--- crawling/crawling/items.py
+++ crawling/crawling/items.py
@@ -1,9 +1,12 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
 import scrapy
-from scrapy_djangoitem import DjangoItem
-from article.models import Movie
 
 
-class MovieItem(DjangoItem):
-    django_model = Movie
-    image_urls = scrapy.Field()
-    images = scrapy.Field()
+class CrawlingItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
